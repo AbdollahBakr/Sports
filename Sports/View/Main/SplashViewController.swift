7ec79795079788c63bf9790b215f673085cd68f0
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-06-16 22:39:51 +0200

    added launch screen with animation

--- Sports/View/Main/SplashViewController.swift
+++ Sports/View/Main/SplashViewController.swift
@@ -9,14 +9,55 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        imageView.image = UIImage(named: "sportsLogo")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(imageView)
 
         // Do any additional setup after loading the view.
-        Thread.sleep(forTimeInterval: 3)
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        imageView.center = view.center
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5) {
+            self.animate()
+        }
+    }
+    
+    private func animate() {
+        UIView.animate(withDuration: 2) {
+            let size = self.view.frame.size.width * 3
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            
+            self.imageView.frame = CGRect(
+            x: -(diffX/2),
+            y: diffY/2,
+            width: size,
+            height: size
+            )
 
+        }
+        UIView.animate(withDuration: 1, animations: {
+            self.imageView.alpha = 0
+        }, completion: { done in
+            if done {
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.3) {
+                    let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController")
+                    homeViewController?.modalTransitionStyle = .crossDissolve
+                    homeViewController?.modalPresentationStyle = .fullScreen
+                    self.present(homeViewController!, animated: true)
+                }
+            }
+        })
+    }
     /*
     // MARK: - Navigation
 
